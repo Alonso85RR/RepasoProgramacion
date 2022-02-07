55c3c1e6d3e16961d1cbb24b2d56ba53c55ce926
Alonso85RR <alonsoriosrizo@gmail.com>
2022-02-07 16:49:09 +0100

    Ejercico con bucles. Eliminados .js

--- src/repasoFor.ts
+++ src/repasoFor.ts
@@ -1,36 +1,6 @@
 
-// Ejercicio2
+
 import { numeroParImpar } from "./repasoIf";
-function obtenerContinenteDePais(pais:string) : string{
-    // "España, Francia"
-    // "Key": "Valor"
-    // 2 - Contenido de la funcion
-    let continentesPaises = [
-        {paises: ["Espana", "Francia", "Italia", "Belgica"], continente: "Europa"},
-        {paises:  ["Sudafrica", "Angola", "Argelia", "Camerun"], continente: "Africa"},
-        {paises: ["Chile", "EEUU", "Bahamas", "Canada", "Argentina"], continente: "America"},
-        {paises: ["China", "Japon", "Indonesia", "Vietnam"], continente: "Asia"},
-        {paises:["Nueva zelanda", "Australia", "Papua Nueva Guinea", "Tonga"], continente: "Oceania"}, 
-    ]
-    let i =0;
-    while ( i < continentesPaises.length)   {
-        if (continentesPaises[i].paises.includes(pais)){
-            // 3- Retorno o salida de la funcion
-            return `El pais ${pais} pertenece al continente ${continentesPaises[i].continente}`;
-            
-        }
-        i++;
-    }
-    
-
-   
-   
-    return `El pais ${pais} no se ha encontrado en ningún continente`;
-
-}
-// 1- Valores de entrada - Parametros 
-console.log(obtenerContinenteDePais("Espana"));
-
 function obtenerNumerosImpares(numero : number) : void{
   
     /*for ( let numeroImpar = 1;  numeroImpar<=numero; numeroImpar = numeroImpar+2){
@@ -47,10 +17,14 @@ function obtenerNumerosImpares(numero : number) : void{
 
 // Ejercicio 4
 function revertirArray(arrayRecibido: string[]){
-    return arrayRecibido.reverse();
+    let arrayNuevo = [];
+    for (var i = arrayRecibido.length - 1; i >= 0; i--) {
+            arrayNuevo.push(arrayRecibido[i]);
+        }
+    return arrayNuevo;
 }
 
-// console.log(revertirArray(["Uno", "Dos", "Tres"]));
+console.log(revertirArray(["Uno", "Dos", "Tres"]));
 
 // Ejercicio 5
 
